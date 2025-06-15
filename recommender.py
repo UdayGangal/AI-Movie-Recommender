8fa2f4ca92c87a4a516d2ed69818d3bc79bdbfa5
UdayGangal <udaygangal2006@gmail.com>
2025-06-15 18:56:05 +0530

    Update recommender.py

--- recommender.py
+++ recommender.py
@@ -1,15 +1,15 @@
 import pandas as pd 
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.metrics.pairwise import linear_kernel
 
 class movierecommender:
- def __intit__(self,csv_path="movies.csv"):
+ def __init__(self,csv_path="movies.csv"):
         self.movies=pd.read_csv(csv_path)
         self.movies['genres']=self.movies['genres'].fillna('').str.replace('|','',regex=False)
         tfidf=TfidfVectorizer(stop_words="english")
         genre_vector=tfidf.fit_transform(self.movies['genres'])
-        self.similarity=cosine_similarity(genre_vector)
-        self.title_index = pd.Series(self.movies.index , index=self.movies['titles']).drop_duplicates()
+        self.similarity=linear_kernel(genre_vector)
+        self.title_index = pd.Series(self.movies.index , index=self.movies['title']).drop_duplicates()
 
 
  def recommend(self,movie_title,top_n=5):
