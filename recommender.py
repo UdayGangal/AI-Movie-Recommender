759239d4d759b861f4ceda737976658d0ce19405
UdayGangal <udaygangal2006@gmail.com>
2025-06-15 14:36:26 +0530

    Update recommender.py

--- recommender.py
+++ recommender.py
@@ -5,4 +5,7 @@ from sklearn.metrics.pairwise import cosine_similarity
 def __int__(self,csv_path="movies.csv")
     self.movies=pd.read_csv(csv_path)
     self.movies['genres']=self.movie['genres'].fillna('').str.replace('|','',regex=FALSE)
+    tfidf=TfidfVectorizer(stop_words="english")
+    genre_vector=tfidf.fit_transform(self.movies['genres'])
+    self.similarirty=cosine_similarity(genre_vector)
     
