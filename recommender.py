7a7b766b34b85656b1bf918fb66443b2f3a71eb0
UdayGangal <udaygangal2006@gmail.com>
2025-06-15 16:15:27 +0530

    Update recommender.py

--- recommender.py
+++ recommender.py
@@ -2,18 +2,22 @@ import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
-def __int__(self,csv_path="movies.csv")
-    self.movies=pd.read_csv(csv_path)
-    self.movies['genres']=self.movie['genres'].fillna('').str.replace('|','',regex=FALSE)
-    tfidf=TfidfVectorizer(stop_words="english")
-    genre_vector=tfidf.fit_transform(self.movies['genres'])
-    self.similarirty=cosine_similarity(genre_vector)
-    self.title_index=pd.Series(self.movies.index , index=self.movies['titles']).drop_duplicates()
+class movierecommender:
+ def __intit__(self,csv_path="movies.csv"):
+        self.movies=pd.read_csv(csv_path)
+        self.movies['genres']=self.movies['genres'].fillna('').str.replace('|','',regex=False)
+        tfidf=TfidfVectorizer(stop_words="english")
+        genre_vector=tfidf.fit_transform(self.movies['genres'])
+        self.similarity=cosine_similarity(genre_vector)
+        self.title_index = pd.Series(self.movies.index , index=self.movies['titles']).drop_duplicates()
 
 
-def recommend(self,movie_title,top_n=5)
+ def recommend(self,movie_title,top_n=5):
     index=self.title_index.get(movie_title)
-    if index is None:
-    return []
-similar_movies=list(enumerate(self.similarity[index]))
-similar_movies=sorted(similar_movies , key=lambda x:x[1] , reverse true)[1:top_n+1]
+    similar_movies=list(enumerate(self.similarity[index]))
+    similar_movies=sorted(similar_movies , key=lambda x:x[1] , reverse=True)[1:top_n+1]
+    recommend=[i[0] for i in similar_movies]
+    return self.movies['title'].iloc[recommend].tolist()
+
+ def get_all_titles(self):
+        return self.movies['title'].sort_values().tolist()
